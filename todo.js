7312f34804e1ac4797eafb7e4da6a724e27c1974
zubai-khanzada <zubairkhanzada7@gmail.com>
2024-03-21 21:55:23 +0500

    Added Edit Functionality

--- todo.js
+++ todo.js
@@ -2,16 +2,35 @@ const taskField = document.getElementById("input");
 const taskList = document.getElementById("taskList");
 
 function add() {
-    if (taskField.value == "") {
+    if (taskField.value === "") {
         alert("Please Enter Task");
     } else {
         let newElement = document.createElement("li");
-        newElement.innerHTML=`${taskField.value}<img src="images/delete.svg" alt="delete">`;
-        taskList.appendChild(newElement);
+        newElement.innerHTML=`
+            <span class="task-text">${taskField.value}</span>
+            <img class="edit-task" src="images/edit.svg" alt="edit">
+            <img class="delete-task" src="images/delete.svg" alt="delete">
+        `;
+        const firstTask = taskList.firstChild;
+        taskList.insertBefore(newElement, firstTask);
         taskField.value = "";
-        newElement.querySelector("img").addEventListener("click", remove);
-        function remove() {
-            newElement.remove();
-        }
+        const deleteButton = newElement.querySelector(".delete-task");
+        const editButton = newElement.querySelector(".edit-task");
+        deleteButton.addEventListener("click", remove);
+        editButton.addEventListener("click", edit);
+    }
+}
+
+function remove() {
+    const listItem = this.parentNode;
+    listItem.remove();
+}
+
+function edit() {
+    const listItem = this.parentNode;
+    const taskTextElement = listItem.querySelector(".task-text");
+    const newText = prompt("Edit Task", taskTextElement.textContent);
+    if (newText !== null) {
+        taskTextElement.textContent = newText;
     }
 }
